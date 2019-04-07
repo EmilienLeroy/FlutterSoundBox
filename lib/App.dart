f17fb173015eda0f59908ff02b45e3e84498938e
Emilien <admin@emilienleroy.fr>
2019-04-07 12:48:54 +0200

    add: splash screen

--- lib/App.dart
+++ lib/App.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_sound_box/screens/home.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 //main class
 class App extends StatelessWidget {
@@ -11,6 +12,25 @@ class App extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
       statusBarColor: Color(0xfff08114),
     ));
+
+    var splashScreen =  new SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: new HomeScreen(),
+        title: Text('DBZ QUEBECOIS',style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            shadows:
+            [
+              new Shadow(color: Colors.black45, offset: new Offset(1, 2), blurRadius: 5)
+            ]
+        )),
+        backgroundColor: Color(0xfff08114),
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.white
+    );
+
     //generate a material app
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -18,7 +38,7 @@ class App extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Color(0xfff08114),
       ),
-      home: new HomeScreen(),
+      home: splashScreen,
       routes: <String, WidgetBuilder>{
         // define the routes
       },
