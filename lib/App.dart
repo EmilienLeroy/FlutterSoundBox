1c3250d709702188153d5b20b2c9a4e6d1316e12
Emilien <admin@emilienleroy.fr>
2019-04-07 12:21:48 +0200

    add: status bar color

--- lib/App.dart
+++ lib/App.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_sound_box/screens/home.dart';
 
 //main class
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    //init the status bar color
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Color(0xfff08114),
+    ));
     //generate a material app
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
