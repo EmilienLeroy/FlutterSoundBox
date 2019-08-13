45f754578ff3d63e361442e9d463f8bf88f71ba0
Emilien <admin@emilienleroy.fr>
2019-08-13 22:45:25 +0200

    :sparkles: Add sekai sound assets

--- lib/data/sounds.dart
+++ lib/data/sounds.dart
@@ -3,143 +3,39 @@ import 'package:flutter/material.dart';
 
 final List<Sound> sounds = [
   new Sound(
-    title: "D'accord",
-    subtitle: "Piccolo",
-    img: new AssetImage("assets/accord.jpg"),
-    soundUrl: 'accord.mp3'
+      title: "C'est moi",
+      subtitle: "Pokemon0",
+      img: new AssetImage("assets/cestmoi.png"),
+      soundUrl: 'cestmoi.mp3'
   ),
   new Sound(
-      title: "Ah",
-      subtitle: "Bulma",
-      img: new AssetImage("assets/ah.jpg"),
-      soundUrl: 'ah.mp3'
+      title: "Padawan",
+      subtitle: "Star Wars",
+      img: new AssetImage("assets/padawan.png"),
+      soundUrl: 'padawan.mp3'
   ),
   new Sound(
-      title: "Ah 2",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/ah2.jpg"),
-      soundUrl: 'ah2.mp3'
+      title: "Victoire",
+      subtitle: "Pokemon",
+      img: new AssetImage("assets/victoire.png"),
+      soundUrl: 'victoire.mp3'
   ),
   new Sound(
-      title: "Haut les coeurs",
-      subtitle: "Radditz",
-      img: new AssetImage("assets/au_les_coeurs.jpg"),
-      soundUrl: 'au_les_coeurs.mp3'
-  ),new Sound(
-      title: "Babysitter",
-      subtitle: "Krillin",
-      img: new AssetImage("assets/babysister.jpg"),
-      soundUrl: 'babysister.mp3'
-  ),new Sound(
-      title: "Bonjour",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/bonjour.jpg"),
-      soundUrl: 'bonjour.mp3'
-  ),new Sound(
-      title: "Bonjour tout le  monde",
-      subtitle: "Sangohan",
-      img: new AssetImage("assets/bonjour_tous.jpg"),
-      soundUrl: 'bonjour_tous.mp3'
-  ),new Sound(
-      title: "Dit bonjour",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/dit_bonjour.jpg"),
-      soundUrl: 'dit_bonjour.mp3'
-  ),new Sound(
-      title: "Du futur",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/futur.jpg"),
-      soundUrl: 'futur.mp3'
-  ),new Sound(
-      title: "Hey",
-      subtitle: "Bulma",
-      img: new AssetImage("assets/hey.jpg"),
-      soundUrl: 'hey.mp3'
-  ),new Sound(
-      title: "Incroyable",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/incroyable.jpg"),
-      soundUrl: 'incroyable.mp3'
-  ),new Sound(
-      title: "Je sais",
-      subtitle: "Piccolo",
-      img: new AssetImage("assets/je_sais.jpg"),
-      soundUrl: 'je_sais.mp3'
-  ),new Sound(
-      title: "Kakaro",
-      subtitle: "Raddits",
-      img: new AssetImage("assets/kakaro.jpg"),
-      soundUrl: 'kakaro.mp3'
-  ),new Sound(
-      title: "Mignon",
-      subtitle: "Bulma",
-      img: new AssetImage("assets/mignon.jpg"),
-      soundUrl: 'mignon.mp3'
-  ),new Sound(
-      title: "Ca sent la mort",
-      subtitle: "Tortue Genial",
-      img: new AssetImage("assets/mort.jpg"),
-      soundUrl: 'mort.mp3'
-  ),new Sound(
-      title: "Oh",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/oh.jpg"),
-      soundUrl: 'oh.mp3'
-  ),new Sound(
-      title: "J'ai peur",
-      subtitle: "Nappa",
-      img: new AssetImage("assets/peur.jpg"),
-      soundUrl: 'peur.mp3'
-  ),new Sound(
-      title: "Puissance",
-      subtitle: "Radditz",
-      img: new AssetImage("assets/puissance.jpg"),
-      soundUrl: 'puissance.mp3'
-  ),new Sound(
-      title: "Une queue",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/queue.jpg"),
-      soundUrl: 'queue.mp3'
-  ),new Sound(
-      title: "Sangoku",
-      subtitle: "Bulma",
-      img: new AssetImage("assets/sangoku.jpg"),
-      soundUrl: 'sangoku.mp3'
-  ),new Sound(
-      title: "Sayen",
-      subtitle: "Radditz",
-      img: new AssetImage("assets/sayen.jpg"),
-      soundUrl: 'sayen.mp3'
-  ),new Sound(
-      title: "Sayen 2",
-      subtitle: "Voix Off",
-      img: new AssetImage("assets/sayen2.jpg"),
-      soundUrl: 'sayen2.mp3'
-  ),new Sound(
-      title: "Je viens du futur",
-      subtitle: "Trunks",
-      img: new AssetImage("assets/trunks.jpg"),
-      soundUrl: 'trunks.mp3'
-  ),new Sound(
-      title: "Vegeta",
-      subtitle: "Trunks",
-      img: new AssetImage("assets/vegeta.jpg"),
-      soundUrl: 'vegeta.mp3'
-  ),new Sound(
-      title: "Vegeta 2",
-      subtitle: "Radditz",
-      img: new AssetImage("assets/vegeta2.jpg"),
-      soundUrl: 'vegeta2.mp3'
-  ),new Sound(
-      title: "Vegeta3",
-      subtitle: "Voix Off",
-      img: new AssetImage("assets/vegeta3.jpg"),
-      soundUrl: 'vegeta3.mp3'
-  ),new Sound(
-      title: "Vraiment",
-      subtitle: "Sangoku",
-      img: new AssetImage("assets/vraiment.jpg"),
-      soundUrl: 'vraiment.mp3'
-  )
-
+      title: "PooKeemon",
+      subtitle: "Pokemon",
+      img: new AssetImage("assets/pookeemon.png"),
+      soundUrl: 'pookeemon.mp3'
+  ),
+  new Sound(
+      title: "Quoiiii",
+      subtitle: "Star Wars",
+      img: new AssetImage("assets/quoi.png"),
+      soundUrl: 'quoi.mp3'
+  ),
+  new Sound(
+      title: "Hypeeer",
+      subtitle: "Star Wars",
+      img: new AssetImage("assets/hyper.png"),
+      soundUrl: 'hyper.mp3'
+  ),
 ];
